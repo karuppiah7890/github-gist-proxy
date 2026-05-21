6b0c59e652aa4d9bbf5b85cf21c79042216e969b
Karuppiah Natarajan <karuppiah7890@gmail.com>
2026-05-21 20:57:43 +0530

    add newlines after every error log

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func usernameHandler(w http.ResponseWriter, r *http.Request) {
 		pageInput := r.URL.Query().Get("page")
 		pageNumber, err := strconv.Atoi(pageInput)
 		if err != nil {
-			fmt.Printf("an error occurred while converting %s page input to number: %v", pageInput, err)
+			fmt.Printf("an error occurred while converting %s page input to number: %v\n\n", pageInput, err)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Header().Add("Content-Type", "application/json")
 			fmt.Fprintf(w, "{\"message\": \"page '%s' is not a number\"}", pageInput)
@@ -53,7 +53,7 @@ func usernameHandler(w http.ResponseWriter, r *http.Request) {
 	gists, err := githubgist.NewClient().ListGists(username, page)
 	if err != nil {
 		if e, ok := errors.AsType[githubgist.UserNotFoundErr](err); ok {
-			fmt.Printf("an error occurred while listing gists of the user %s: %v. it seems like a user not found error: %v", username, err, e)
+			fmt.Printf("an error occurred while listing gists of the user %s: %v. it seems like a user not found error: %v\n\n", username, err, e)
 			w.WriteHeader(http.StatusNotFound)
 			w.Header().Add("Content-Type", "application/json")
 			fmt.Fprintf(w, "{\"message\": \"username '%s' does not exist\"}", username)
@@ -72,7 +72,7 @@ func usernameHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func reportUnexpectedError(err error, w http.ResponseWriter, username string) {
-	fmt.Printf("an unexpected error occurred while listing gists of the user %s: %v", username, err)
+	fmt.Printf("an unexpected error occurred while listing gists of the user %s: %v\n\n", username, err)
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Add("Content-Type", "application/json")
 	fmt.Fprintf(w, "{\"message\": \"an error occurred while getting the gists of the username '%s'. please contact support or the api admin to resolve the issue\"}", username)

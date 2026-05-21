01d0f082cfb45bc0124fa3b3339acdaa85a6b9c8
Karuppiah Natarajan <karuppiah7890@gmail.com>
2026-05-21 20:29:51 +0530

    simplify w.Write([]byte(fmt.Sprintf()) with fmt.Fprintf()

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func usernameHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("an error occurred while converting %s page input to number: %v", pageInput, err)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Header().Add("Content-Type", "application/json")
-			w.Write([]byte(fmt.Sprintf("{\"message\": \"page '%s' is not a number\"}", pageInput)))
+			fmt.Fprintf(w, "{\"message\": \"page '%s' is not a number\"}", pageInput)
 			return
 		}
 		page = pageNumber
@@ -56,7 +56,7 @@ func usernameHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("an error occurred while listing gists of the user %s: %v. it seems like a user not found error: %v", username, err, e)
 			w.WriteHeader(http.StatusNotFound)
 			w.Header().Add("Content-Type", "application/json")
-			w.Write([]byte(fmt.Sprintf("{\"message\": \"username '%s' does not exist\"}", username)))
+			fmt.Fprintf(w, "{\"message\": \"username '%s' does not exist\"}", username)
 		} else {
 			reportUnexpectedError(err, w, username)
 		}
@@ -75,5 +75,5 @@ func reportUnexpectedError(err error, w http.ResponseWriter, username string) {
 	fmt.Printf("an unexpected error occurred while listing gists of the user %s: %v", username, err)
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Add("Content-Type", "application/json")
-	w.Write([]byte(fmt.Sprintf("{\"message\": \"an error occurred while getting the gists of the username '%s'. please contact support or the api admin to resolve the issue\"}", username)))
+	fmt.Fprintf(w, "{\"message\": \"an error occurred while getting the gists of the username '%s'. please contact support or the api admin to resolve the issue\"}", username)
 }

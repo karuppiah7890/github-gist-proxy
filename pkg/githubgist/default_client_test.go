87514b49b9c2593f117ad9be92d33fb73776f55c
Karuppiah Natarajan <karuppiah7890@gmail.com>
2026-05-21 19:19:55 +0530

    add test for username with space

--- pkg/githubgist/default_client_test.go
+++ pkg/githubgist/default_client_test.go
@@ -54,5 +54,26 @@ func TestListGists(t *testing.T) {
 		}
 	})
 
-	// TODO: Check what happens when there are characters like space in the username? As the current implementation does string interpolation of the username inside the URL as is - whatever the user gives as input
+	// What happens when there are characters like space in the username?
+	// As the current implementation does string interpolation of the username
+	// inside the URL as is - whatever the user gives as input.
+	// Looks like github usernames cannot have space in them - so,
+	// any username with space in it should give user not found
+	t.Run("space in username", func(t *testing.T) {
+		username := "something lol"
+		gists, err := githubgist.NewClient().ListGists(username)
+		if err == nil {
+			t.Errorf("expected error while listing gists of the user that is supposed to be non existent - %s, but got no error", username)
+		}
+
+		_ = gists
+
+		if e, ok := errors.AsType[githubgist.UserNotFoundErr](err); ok {
+			_ = e
+			// For debugging
+			// t.Logf("as expected, we got an error. error is %v", e)
+		} else {
+			t.Errorf("expected a specific kind of error while listing gists of a user that is supposed to be non existent - user: %s, but got a different error: error type: %T, error: %v", username, err, err)
+		}
+	})
 }
